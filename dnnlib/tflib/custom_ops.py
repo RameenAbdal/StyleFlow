696cb3ad25aef775fc901331e6cfbf4fcb26e241
Justin John <34035011+justinjohn0306@users.noreply.github.com>
2021-01-19 11:56:50 +0530

    Add files via upload

--- dnnlib/tflib/custom_ops.py
+++ dnnlib/tflib/custom_ops.py
@@ -25,10 +25,7 @@ do_not_hash_included_headers = False # Speed up compilation by assuming that hea
 verbose = True # Print status messages to stdout.
 
 compiler_bindir_search_path = [
-    'C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.14.26428/bin/Hostx64/x64',
-    'C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.16.27023/bin/Hostx64/x64',
-    'C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/MSVC/14.23.28105/bin/Hostx64/x64',
-    'C:/Program Files (x86)/Microsoft Visual Studio 14.0/vc/bin',
+    'C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.10.25017/bin/HostX64/x64',
 ]
 
 #----------------------------------------------------------------------------
